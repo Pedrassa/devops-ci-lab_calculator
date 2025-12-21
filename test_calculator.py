2e9afb8a1464a5180e8f011ca8f1643195ef6f72
Pedrassa <49522692+Pedrassa@users.noreply.github.com>
2025-12-21 22:11:16 +0100

    Add test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,9 @@
+# test_calculator.py
+import calculator
+
+def test_add():
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-1, 1) == 0
+
+def test_subtract():
+    assert calculator.subtract(5, 3) == 2
